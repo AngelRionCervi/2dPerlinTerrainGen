3fc94a041eb3cd29bf3d09f6d19cc665b8651096
AngelRionCervi <a.rioncervi@gmail.com>
2019-08-04 03:49:50 +0200

    multiple maps rendered

--- js/class/map/mapGen.js
+++ js/class/map/mapGen.js
@@ -2,35 +2,107 @@ class Map {
 	constructor(){
 		this.biomeTypes = ["sea", "desert", "forest"];
 		this.biomeStart = "forest";
-		this.tileSize = 5;
+		this.tileSize = 8;
 		this.biomes = new Biomes();
 		this.circleRadRatio = 3.5; //bigger means smaller fadeoff circle
-		this.fadeOffRatio = 500; // bigger means smoother
+		this.fadeOffRatio = 100; // bigger means smoother
 
 	}
 
 	generate(){
 	
-		let canvas = document.getElementById("canvas");
-		let ctx = canvas.getContext("2d");
+		let canvasGen = document.getElementById("canvasGen");
+        canvasGen.width = 12000;
+        canvasGen.height = 10000;
+		let ctx = canvasGen.getContext("2d");
 
 		noise.seed(Math.random());
-		let rndm = _.random(50, 250);
-		let riverZoom = _.random(80, 150);
-		let elevationOffset = _.random(0.6, 1.2)
-		let angle = 0;
-		let radius = canvas.width/this.circleRadRatio;
-		for (let x = 0; x < canvas.width; x += this.tileSize) {
-  			for (let y = 0; y < canvas.height; y += this.tileSize) {
 
-  				angle ++;
-  				if(angle === 361) angle = 0;
+		
+		//let angle = 0;
+		let radius = canvasGen.width/this.circleRadRatio;
+        let fill = ['red', 'green', 'blue', 'orange', 'purple', 'black'];
+
+        let primeRectCoor = [];
+
+            let rndmRectXLength = _.random(150, 300);
+            let rndmRectYLength = _.random(150, 300);
+            let rndmRectXStart = _.random(100, canvasGen.width - 100 - rndmRectXLength);
+            let rndmRectYStart = _.random(100, canvasGen.height - 100 - rndmRectYLength);
+            //let rndmSide = _.random(1, 1);
+            let sqr1 = [rndmRectXStart, rndmRectYStart, rndmRectXLength, rndmRectYLength]
+            let sqr2 = [0, 0, rndmRectXStart + rndmRectXLength, rndmRectYStart];
+            let sqr3 = [0, rndmRectYStart, rndmRectXStart, canvasGen.height - rndmRectYStart];
+            let sqr4 = [rndmRectXStart, rndmRectYStart + rndmRectYLength, canvasGen.width - rndmRectXStart, canvasGen.height - rndmRectYStart];
+            let sqr5 = [rndmRectXStart + rndmRectXLength, 0, canvasGen.width - rndmRectXLength - rndmRectXStart, rndmRectYStart + rndmRectYLength];
+/*
+            ctx.fillStyle = fill[0];
+            ctx.fillRect(rndmRectXStart, rndmRectYStart, rndmRectXLength, rndmRectYLength);
+            ctx.fillStyle = fill[1];
+            ctx.fillRect(sqr2[0], sqr2[1], sqr2[2], sqr2[3]);
+            ctx.fillStyle = fill[2];
+            ctx.fillRect(sqr3[0], sqr3[1], sqr3[2], sqr3[3]);
+            ctx.fillStyle = fill[3];
+            ctx.fillRect(sqr4[0], sqr4[1], sqr4[2], sqr4[3]);
+            ctx.fillStyle = fill[4];
+            ctx.fillRect(sqr5[0], sqr5[1], sqr5[2], sqr5[3]);*/
+
+
+            primeRectCoor.push(sqr1, sqr2, sqr3, sqr4, sqr5);
+
+            primeRectCoor.forEach((v, i, a) => {
+
+                let subRectCoor = [];
+                
+                let subRndmRectXlength = _.random(v[2]/4, v[2]/2.5);
+                if(subRndmRectXlength < 100) subRndmRectXlength = 100;
+
+                let subRndmRectYlength = _.random(v[3]/4, v[3]/2.5);
+                if(subRndmRectYlength < 100) subRndmRectYlength = 100;
+
+                let subRndmRectXstart = _.random(50, v[2] - 50 - subRndmRectXlength);
+                let subRndmRectYstart = _.random(50, v[3] - 50 - subRndmRectYlength);
+
+                let subSqr1 = [subRndmRectXstart, subRndmRectYstart, subRndmRectXlength, subRndmRectYlength];
+                let subSqr2 = [0, 0, subRndmRectXstart + subRndmRectXlength, subRndmRectYstart];
+                console.log(subSqr2)
+                let subSqr3 = [0, subRndmRectYstart, subRndmRectXstart, v[3]-subRndmRectYstart+subRndmRectYlength];
+                let subSqr4 = [subRndmRectXstart, subRndmRectYstart + subRndmRectYlength, v[2] - subRndmRectXstart, v[3] - subRndmRectYstart];
+                let subSqr5 = [subRndmRectXstart + subRndmRectXlength, 0, v[2] - subRndmRectXlength - subRndmRectXstart, subRndmRectYlength + subRndmRectYstart];
+
+                subRectCoor.push(subSqr1, subSqr2, subSqr3, subSqr4, subSqr5);
+        
+/*
+                ctx.fillStyle = fill[0];
+                ctx.fillRect(v[0] + subSqr1[0], v[1] + subSqr1[1], subSqr1[2], subSqr1[3]);
+                ctx.fillStyle = fill[1];
+                ctx.fillRect(v[0] + subSqr2[0], v[1] + subSqr2[1], subSqr2[2], subSqr2[3]);
+                ctx.fillStyle = fill[2];
+                ctx.fillRect(v[0] + subSqr3[0], v[1] + subSqr3[1], subSqr3[2], subSqr3[3]);
+                ctx.fillStyle = fill[3];
+                ctx.fillRect(v[0] + subSqr4[0], v[1] + subSqr4[1], subSqr4[2], subSqr4[3]);
+                ctx.fillStyle = fill[4];
+                ctx.fillRect(v[0] + subSqr5[0], v[1] + subSqr5[1], subSqr5[2], subSqr5[3]);*/
+                
+            
+
+                subRectCoor.forEach((v2, i2, a2)=>{ 
+
+                    let rndm = _.random(150, 500);
+                    let riverZoom = _.random(80, 150);
+                    let elevationOffset = _.random(0.3, 2)
+
+		        for (let x = 0; x < v2[2]; x += this.tileSize) {
+  			       for (let y = 0; y < v2[3]; y += this.tileSize) {
+
+  				//angle ++;
+  				//if(angle === 361) angle = 0;
 
   				let noiseOffset = 0;
 
-  				if(((x-canvas.width/2)*(x-canvas.width/2)) + ((y - canvas.height/2)*(y - canvas.height/2)) > (radius*radius)) {
+  				if(((x-v2[2]/2) * (x-v2[2]/2)) + ((y - v2[3]/2)*(y - v2[3]/2)) > (radius*radius)) {
 
-					noiseOffset = Math.round((((x-canvas.width/2)*(x-canvas.width/2)) + ((y - canvas.height/2)*(y - canvas.height/2)) - (radius*radius)) / this.fadeOffRatio);
+					noiseOffset = Math.round((((x-v2[2]/2) * (x-v2[2]/2)) + ((y - v2[3]/2) * (y - v2[3]/2)) - (radius*radius)) / this.fadeOffRatio);
   					//console.log(noiseOffset)
 
   				}
@@ -57,11 +129,11 @@ class Map {
     			let tyleType = this.biomes.getBiomeByNoise(tileElevation, tileClimate, tileRivers);
 
 
-    			ctx.globalAlpha = 1;
+    			
     			ctx.fillStyle = tyleType.getBiomeInfo().color
     			//ctx.fillStyle = 'rgb(' + tileElevation + ',' + tileElevation + ',' + tileElevation + ')';
-    			ctx.fillRect(x, y, this.tileSize, this.tileSize)
-
+    		    ctx.fillRect(Math.round(x+v[0]+v2[0]), Math.round(y+v[1]+v2[1]), this.tileSize, this.tileSize)
+/*
     			ctx.globalAlpha = 0.0;
     			ctx.fillStyle = 'rgba(' + tileClimate + ',' + tileClimate + ',' + tileClimate + ',' + tileClimate/20 + ')';
     			ctx.fillRect(x, y, this.tileSize, this.tileSize)
@@ -76,13 +148,19 @@ class Map {
 
     			ctx.globalAlpha = 0;
     			ctx.fillStyle = 'purple';
-    			ctx.fillRect(canvas.width/2, canvas.height/2, 4, 4)
+    			ctx.fillRect(v2[2]/2, v2[3]/2, 4, 4)*/
 
     		
-    			//ctx.fillRect(Math.abs(Math.ceil(canvas.width/2 + canvas.width/2.2*Math.cos(angle))), Math.abs(Math.ceil(canvas.height/2 + canvas.height/2.2*Math.sin(angle))), this.tileSize, this.tileSize);
-    			//image[x][y].r = Math.abs(value) * 256; // Or whatever. Open demo.html to see it used with canvas.
+    			//ctx.fillRect(Math.abs(Math.ceil(v2[2]/2 + canvasGen.width/2.2*Math.cos(angle))), Math.abs(Math.ceil(canvasGen.height/2 + canvasGen.height/2.2*Math.sin(angle))), this.tileSize, this.tileSize);
+    			//image[x][y].r = Math.abs(value) * 256; // Or whatever. Open demo.html to see it used with canvasGen.
     			
-  			}
+  			
+            
+            }
+
 		}
+
+        })
+        })
 	}
 }
--- js/gameStart.js
+++ js/gameStart.js
@@ -1,2 +1,147 @@
 let map = new Map();
 map.generate();
+
+
+let canvas = document.getElementById('canvas');
+let canvasGen = document.getElementById('canvasGen');
+
+let mapImage = new Image();
+mapImage.id = "mapImage"
+mapImage.src = canvasGen.toDataURL();
+
+window.onload = function(){		
+	
+	let ctx = canvas.getContext('2d');
+	ctx .imageSmoothingEnabled = false;
+	trackTransforms(ctx);
+	
+	function redraw(){
+
+      // Clear the entire canvas
+      let p1 = ctx.transformedPoint(0,0);
+      let p2 = ctx.transformedPoint(canvas.width,canvas.height);
+      ctx.clearRect(p1.x,p1.y,p2.x-p1.x,p2.y-p1.y);
+
+      ctx.save();
+      ctx.setTransform(1,0,0,1,0,0);
+      ctx.clearRect(0,0,canvas.width,canvas.height);
+      ctx.restore();
+
+      ctx.drawImage(mapImage, 0, 0);
+
+  }
+  redraw();
+
+  let lastX=canvas.width/2, lastY=canvas.height/2;
+
+  let dragStart,dragged;
+
+  canvas.addEventListener('mousedown',function(evt){
+  	document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+  	lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+  	lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+  	dragStart = ctx.transformedPoint(lastX,lastY);
+  	dragged = false;
+  },false);
+
+  canvas.addEventListener('mousemove',function(evt){
+  	lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+  	lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+  	dragged = true;
+  	if (dragStart){
+  		let pt = ctx.transformedPoint(lastX,lastY);
+  		ctx.translate(pt.x-dragStart.x,pt.y-dragStart.y);
+  		redraw();
+  	}
+  },false);
+
+  canvas.addEventListener('mouseup',function(evt){
+  	dragStart = null;
+  },false);
+
+  let scaleFactor = 1.1;
+
+  let zoom = function(clicks){
+  	let pt = ctx.transformedPoint(lastX,lastY);
+  	ctx.translate(pt.x,pt.y);
+  	let factor = Math.pow(scaleFactor,clicks);
+  	ctx.scale(factor,factor);
+  	ctx.translate(-pt.x,-pt.y);
+  	redraw();
+  }
+
+  let handleScroll = function(evt){
+  	let delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
+  	if (delta) zoom(delta);
+  	return evt.preventDefault() && false;
+  };
+  
+  canvas.addEventListener('DOMMouseScroll',handleScroll,false);
+  canvas.addEventListener('mousewheel',handleScroll,false);
+};
+
+
+
+// Adds ctx.getTransform() - returns an SVGMatrix
+// Adds ctx.transformedPoint(x,y) - returns an SVGPoint
+function trackTransforms(ctx){
+	let svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+	let xform = svg.createSVGMatrix();
+	ctx.getTransform = function(){ return xform; };
+
+	let savedTransforms = [];
+	let save = ctx.save;
+	ctx.save = function(){
+		savedTransforms.push(xform.translate(0,0));
+		return save.call(ctx);
+	};
+	
+	let restore = ctx.restore;
+	ctx.restore = function(){
+		xform = savedTransforms.pop();
+		return restore.call(ctx);
+	};
+
+	let scale = ctx.scale;
+	ctx.scale = function(sx,sy){
+		xform = xform.scaleNonUniform(sx,sy);
+		return scale.call(ctx,sx,sy);
+	};
+	
+	let rotate = ctx.rotate;
+	ctx.rotate = function(radians){
+		xform = xform.rotate(radians*180/Math.PI);
+		return rotate.call(ctx,radians);
+	};
+	
+	let translate = ctx.translate;
+	ctx.translate = function(dx,dy){
+		xform = xform.translate(dx,dy);
+		return translate.call(ctx,dx,dy);
+	};
+	
+	let transform = ctx.transform;
+	ctx.transform = function(a,b,c,d,e,f){
+		let m2 = svg.createSVGMatrix();
+		m2.a=a; m2.b=b; m2.c=c; m2.d=d; m2.e=e; m2.f=f;
+		xform = xform.multiply(m2);
+		return transform.call(ctx,a,b,c,d,e,f);
+	};
+	
+	let setTransform = ctx.setTransform;
+	ctx.setTransform = function(a,b,c,d,e,f){
+		xform.a = a;
+		xform.b = b;
+		xform.c = c;
+		xform.d = d;
+		xform.e = e;
+		xform.f = f;
+		return setTransform.call(ctx,a,b,c,d,e,f);
+	};
+	
+	let pt  = svg.createSVGPoint();
+	ctx.transformedPoint = function(x,y){
+		pt.x=x; pt.y=y;
+		return pt.matrixTransform(xform.inverse());
+	}
+}
